fb53ea2257580bc8e711c2613d2db4650a5c3245
Morten Delenk <morten@dark32.cf>
2017-03-18 17:31:06 +0000

    finished libc (kinda)

---
+++ string/mem.cpp
@@ -0,0 +1,42 @@
+#include <string.h>
+void *memset(void* str, int c, size_t n) {
+    for(size_t i=0;i<n;i++) {
+        *((unsigned char*)(str+i))=c;
+    }
+    return str;
+}
+void *memcpy(void* a, const void* b, size_t n) {
+    unsigned char *src = (unsigned char*)b;
+    const unsigned char *dest = (unsigned char*)a;
+    for(int i=0;i<n;i++)
+        dest[i]=src[i];
+    return dest;
+}
+void *memmove(void* a, const void * b, size_t n) {
+    if(a > b)
+        return memcpy(a, b, n);
+    else {
+        unsigned char *src = (unsigned char*)b;
+        const unsigned char *dest = (unsigned char*)a;
+        for(int i=n-1;i;--i)
+            dest[i]=src[i];
+        return dest;
+    }
+}
+void *memcmp(const void* str1, const void* str2, size_t n) {
+    const unsigned char *a=str1, *b=str2;
+    int diff=0;
+    for(int i=0;(i<n) && (!diff); i++) {
+        diff = (short)((unsigned short)a[i])-b[i];
+    }
+    return diff;
+}
+void *memchr(const void*str, int c, size_t n) {
+    const unsigned char* s = str;
+    unsigned char ch=(unsigned char)c;
+    for(int i=0;i<n;i++) {
+        if(s[i] == ch)
+            return s + i;
+    }
+    return nullptr;
+}
---
+++ string/str.cpp
@@ -0,0 +1,81 @@
+#include <string.h>
+char *strcat(char *dest, const char* src) {
+    int start=0;
+    int i=0;
+    while(dest[start++]);
+    while(src[i]) {
+        dest[start+i]=src[i++];
+    }
+    dest[start+i]=src[i];
+    return dest;
+}
+char *strncat(char *dest, const char* src, size_t n) {
+    int start=0;
+    int i=0;
+    while(dest[start++]);
+    while((src[i]) && (src < n)) {
+        dest[start+i]=src[i++];
+    }
+    if(i!=n)
+        dest[start+i]=0;
+    return dest;
+}
+char *strchr(const char *str, int c) {
+    return memchr(str, c, strlen(str));
+}
+int strcmp(const char *s1, const char *s2) {
+    size_t len1 = strlen(s1), len2=strlen(s2);
+    if(len1!=len2)
+        return len1-len2;
+    return memcmp(s1, s2, len1);
+}
+#define MIN(x,y) ((x) < (y) ? (x) : (y))
+int strncmp(const char *s1, const char *s2, size_t len) {
+    size_t len1 = strlen(s1), len2=strlen(s2);
+    if((len1 <= len) && (len1 != len2))
+        return len1 - len2;
+    return memcmp(s1, s2, MIN(len, len2));
+}
+char *strcpy(char *dest, const char *src) {
+    return memmove(dest, src, strlen(src));
+}
+char *strncpy(char *dest, const char *src, size_t n) {
+    return memmove(dest, src, MIN(strlen(src),n));
+}
+size_t strcspn(const char *str1, const char* str2) {
+    size_t length=0;
+    for(int i=0; str1[i]; i++) {
+        for(int j=0; str2[j]; j++) {
+            if(str1[i] == str2[j])
+                return length;
+        }
+        length++;
+    }
+    return length;
+}
+size_t strlen(const char *str) {
+    size_t len=0;
+    while(str[len])
+        len++;
+}
+char *strpbrk(const char *str1, const char *str2) {
+    for(int i=0; str1[i]; i++) {
+        for(int j=0; str[j]; j++) {
+            if(str1[i] == str2[j])
+                return str1+i;
+        }
+    }
+    return NULL;
+}
+size_t strspn(const char *str1, const char *str2) {
+    size_t len=0;
+    for(int i=0; str1[i]; i++) {
+        for(int j=0; str[j]; j++) {
+            if(str1[i] != str2[j])
+                return len;
+        }
+        len++;
+    }
+    return len;
+}
+
