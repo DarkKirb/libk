f31fb367d3abf5327266a7ebfc5b86025da79ee9
Morten Delenk <morten@dark32.cf>
2017-03-18 18:45:55 +0000

    Update stddef.h

--- include/stddef.h
+++ include/stddef.h
@@ -1,8 +1,3 @@
 #pragma once
-#ifndef __ARM__
-typedef unsigned long size_t;
-typedef long ptrdiff_t;
-#else
-typedef unsigned int size_t;
-typedef int ptrdiff_t;
-#endif
+#include <stdint.h>
+#define ptrdiff_t long
